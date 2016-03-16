8459333c508532ecad9a89cde53dc56dd0e56770
Gopal Vaid <gopalvaid@ravis-mac-mini.local>
2016-03-16 10:55:51 +0530

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Gopal.Vaid on 16/03/16.
+//  Copyright © 2016 Gopal.Vaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Gopal.Vaid on 16/03/16.
+//  Copyright © 2016 Gopal.Vaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
